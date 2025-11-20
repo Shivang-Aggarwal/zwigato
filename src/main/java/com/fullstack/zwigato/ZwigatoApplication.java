3b36fef6e3bcd89e009b1c868abebf08985ded65
shivangM3 <shivangaggarwal6208@gmail.com>
2025-11-20 05:51:24 +0530

    Created new project with Spring Initializer

---
+++ src/main/java/com/fullstack/zwigato/ZwigatoApplication.java
@@ -0,0 +1,13 @@
+package com.fullstack.zwigato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZwigatoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZwigatoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fullstack/zwigato/ZwigatoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullstack.zwigato;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZwigatoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

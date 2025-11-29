00ad98a6e8d726f7037773a26f83ab372fbc4e19
shivangM3 <shivangaggarwal6208@gmail.com>
2025-11-29 16:49:46 +0530

    18. Exceptions

---
+++ src/main/java/com/fullstack/zwigato/exception/PaymentProcessingException.java
@@ -0,0 +1,9 @@
+package com.fullstack.zwigato.exception;
+
+public class PaymentProcessingException extends RuntimeException {
+
+    public PaymentProcessingException(String message) {
+        super(message);
+    }
+
+}
